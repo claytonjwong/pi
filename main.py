f0f9050cc777a99f365002463f47bfabdf42e514
Clayton Wong <clayton.wong@ookla.com>
2019-05-23 11:46:28 -0700

    first 1000 digits of pi, printed in chunks of 100 (10x10) digits

---
+++ main.py
@@ -0,0 +1,11 @@
+size = 10
+file = open('1000.txt')
+i = 0
+while True:
+    chunk = file.read( size )
+    if not chunk:
+        break
+    i += 1
+    if 1 < i and i % size == 1:
+        print
+    print(chunk)
